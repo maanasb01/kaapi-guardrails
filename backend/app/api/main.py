50ab4896c83a1d404078e871dba0a57b5487f254
rkritika1508 <rkritika1508@gmail.com>
2025-12-18 12:00:56 +0530

    Setup repo and added bearer token based auth. #2

---
+++ backend/app/api/main.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+from app.api.routes import utils
+
+api_router = APIRouter()
+api_router.include_router(utils.router)
+
+
+# if settings.ENVIRONMENT == "local":
+#     api_router.include_router(private.router)
---
+++ backend/app/api/routes/utils.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+from app.api.deps import AuthDep
+
+router = APIRouter(prefix="/utils", tags=["utils"])
+
+@router.get("/health-check/")
+async def health_check(
+    _: AuthDep
+) -> bool:
+    return True
